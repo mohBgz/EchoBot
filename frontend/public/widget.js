dcb05c00972ab2d15f97900fe74ec16be1b303f3
Mohamed Bouguezine <bouguezine.mohamed@gmail.com>
2025-11-29 13:52:08 +0100

    deploy ready 2

--- frontend/public/widget.js
+++ frontend/public/widget.js
@@ -1,29 +1,30 @@
 (function () {
-  const iframe = document.createElement("iframe");
-  iframe.className = "chat-widget";
+	const iframe = document.createElement("iframe");
+	iframe.className = "chat-widget";
 
-  // Set source
-  iframe.src = "http://localhost:5173"; // or your deployed URL
+	iframe.src =
+		window.location.hostname === "localhost"
+			? "http://localhost:5173"
+			: "https://echobot.mohamedbgz.dev";
 
-  document.body.appendChild(iframe);
+	document.body.appendChild(iframe);
 
+	const overlay = document.getElementById("chat-overlay");
+	console.log(overlay);
 
- const overlay = document.getElementById("chat-overlay");
- console.log(overlay);
+	// Listen for messages from the iframe to toggle overlay
+	window.addEventListener("message", (event) => {
+		if (!event.data || event.data.type !== "TOGGLE_CHAT_OVERLAY") return;
 
-  // Listen for messages from the iframe to toggle overlay
-  window.addEventListener("message", (event) => {
-    if (!event.data || event.data.type !== "TOGGLE_CHAT_OVERLAY") return;
+		if (event.data.isOpen) {
+			overlay.style.display = "block";
+		} else {
+			overlay.style.display = "none";
+		}
+	});
 
-    if (event.data.isOpen) {
-      overlay.style.display = "block";
-    } else {
-      overlay.style.display = "none";
-    }
-  });
-
-  // Close panel if overlay is clicked
-  overlay.addEventListener("click", () => {
-    iframe.contentWindow.postMessage({ type: "CLOSE_PANEL" }, "*");
-  });
+	// Close panel if overlay is clicked
+	overlay.addEventListener("click", () => {
+		iframe.contentWindow.postMessage({ type: "CLOSE_PANEL" }, "*");
+	});
 })();
